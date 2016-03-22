22b6ad8c14d5ef829943f0b554194599b28ee88f
Saurabh Datar <sdatar2@stanford.edu>
2016-03-22 16:03:34 -0700

    Updated

--- analyze.py
+++ analyze.py
@@ -10,6 +10,7 @@ thefile = join(folder, 'classified.csv')
 
 presidents = {}
 
+#Open classified csv
 with open(thefile, 'r') as classified:
 	reader = csv.reader(classified)
 	next(reader)
@@ -22,20 +23,31 @@ with open(thefile, 'r') as classified:
 
 		presidents[univ][gender].append({'name': name, 'term': term})
 
-f_schools = []
-m_schools = []
+f_schools = set()
+m_schools = set()
 
+# for s in f_schools:
+# 	print(s, presidents[s]['F'])
+# 	print('\n')
+
+#Make separate lists for male and female
 for school in presidents:
 	if len(presidents[school]['F']) > 0:
-		f_schools.append(school)
+		f_schools.add(school)
 	else:
-		m_schools.append(school)
+		m_schools.add(school)
 
+#Write a csv with the gender/count data
 with open('data/univ.csv', 'w') as univ:
 	writer = csv.writer(univ)
 	writer.writerow(['university','female_pres', 'count'])
 	
 	for s in f_schools:
-		writer.writerow([s, 'y',len(presidents[s]['F'])])
+		temp = set()		#to ensure one person's name doesn't get repeated and add to count
+
+		for k in range(len(presidents[s]['F'])):
+			temp.add(presidents[s]['F'][k]['name'])
+		writer.writerow([s, 'y',len(temp)])
+	
 	for p in m_schools:
-		writer.writerow([p, 'n',0])
+		writer.writerow([p, 'n', 0])
